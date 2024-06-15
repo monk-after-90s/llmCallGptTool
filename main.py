5e29e6ac4c438d357f083ab78910884fd0f80f8f
90houlaoheshang <907333918@qq.com>
2024-06-15 16:19:41 +0800

    跑通代理中间件，但是不能流式

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Request, logger
+from fastapi.responses import JSONResponse, StreamingResponse
+import httpx
+from loguru import logger
+
+app = FastAPI()
+
+TARGET_URL = "https://qwen2-72b-instruct-gptq-int4.excn.top"
+
+
+@app.middleware("http")
+async def proxy_middleware(request: Request, call_next):
+    logger.debug(f"request: {request.url}")
+    async with httpx.AsyncClient() as client:
+        # 构建目标URL
+        url = f"{TARGET_URL}{request.url.path}"
+
+        # 获取请求方法
+        method = request.method
+
+        # 获取请求头
+        headers = dict(request.headers)
+        headers["host"] = headers["x-forwarded-host"] = TARGET_URL.split("//")[1]
+        # 获取请求体
+        body = await request.body()
+
+        # 发送请求到目标服务
+        response = await client.request(
+            method=method,
+            url=url,
+            headers=headers,
+            content=body,
+            params=request.query_params
+        )
+
+        # 构建响应
+        return StreamingResponse(response.aiter_bytes(), status_code=response.status_code, headers=response.headers)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
